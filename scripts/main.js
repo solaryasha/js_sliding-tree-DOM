ce0a8df298cb3078cc471ee39ce97c2e60fca5d7
solaryasha <solaryasha@gmail.com>
2020-06-15 19:15:33 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,20 @@ tree.addEventListener('click', e => {
     [ ...title.children ].forEach(item => item.classList.toggle('hide'));
   }
 });
+
+tree.onmouseover = function({ target }) {
+  // first child of our target should have extra styling
+  if (target.firstElementChild) {
+    target.style.fontWeight = 'bold';
+
+    // all the other children should have a normal font weight
+    [ ...target.children ]
+      .forEach(element => {
+        element.style.fontWeight = 'normal';
+      });
+  }
+};
+
+tree.onmouseout = function({ target }) {
+  target.style.fontWeight = 'normal';
+};
